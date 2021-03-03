44c90ef58bf7df461362781d7109bf4958571012
Nicole <nzolnier@gmail.com>
2021-03-03 14:10:11 -0300

    login

--- labegram/src/screens/LoginPage/LoginPage.js
+++ labegram/src/screens/LoginPage/LoginPage.js
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import { useForm } from '../../hooks/useForm';
+import { login } from '../../services/user'
 
 function LoginPage() {
+  const { form, onChange, reset } = useForm({ email: "", password: "" })
+  const history = useHistory()
+  const [loading, setLoading] = useState(false)
+
+  const handleSubmission = (e) => {
+    e.preventDefault()
+    login(form, history, setLoading)
+    reset()
+  }
+
   return (
     <div>
-     LoginPage
+      <form onSubmit={handleSubmission}>
+        <input placeholder="email" type="email" onChange={onChange} value={form.email} name="email" />
+        <input placeholder="password" type="password" onChange={onChange} value={form.password} name="password" />
+        <button>Sign up</button>
+      </form>
     </div>
   );
 }
--- labegram/src/services/user.js
+++ labegram/src/services/user.js
@@ -2,6 +2,19 @@ import axios from 'axios'
 import { baseUrl } from '../constants/urls'
 import { goToFeed } from '../router/coordinator'
 
+export const login = (body, history, setLoading) => {
+    setLoading(true)
+    axios.post(`${baseUrl}/user/login`, body).then((res) => {
+      setLoading(false)
+      localStorage.setItem("token", res.data.token)
+      goToFeed(history)
+    }).catch((err) => {
+      setLoading(false)
+      window.alert("Email ou senha incorretos!")
+      console.log(err.message)
+    })
+  }
+
 export const signUp = (body, history, setLoading) => {
     setLoading(true)
     axios.post(`${baseUrl}/user/signup`, body).then((res) => {
